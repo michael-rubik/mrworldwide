e256d77264354065c50d8f08b496cfd91932e8da
michael-rubik <michael.rubik@outlook.at>
2022-02-19 17:14:10 +0100

    import data as pandas dataframe

--- stock_tracker.py
+++ stock_tracker.py
@@ -8,9 +8,9 @@ from helper_functions import *
 from alpha_vantage.timeseries import TimeSeries
 
 
-ts = TimeSeries(key='YOUR_API_KEY')
+ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
 # Get json object with the intraday data and another with  the call's metadata
-data, meta_data = ts.get_intraday('GOOGL')
+data = pd.DataFrame
+data, meta_data = ts.get_daily(symbol = 'GOOGL', outputsize='full')
 
 print("data: ", data)
-print("meta data:", type(meta_data), meta_data)
