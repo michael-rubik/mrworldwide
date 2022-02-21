dabb041775af300e3ebafc3494c9e0176094f320
michael-rubik <michael.rubik@outlook.at>
2022-02-21 12:41:38 +0100

    enable date trimming

--- stock_tracker.py
+++ stock_tracker.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import datetime
 from helper_functions import *
 
 from alpha_vantage.timeseries import TimeSeries
@@ -10,7 +11,15 @@ from alpha_vantage.timeseries import TimeSeries
 
 ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
 # Get json object with the intraday data and another with  the call's metadata
-data = pd.DataFrame
 data, meta_data = ts.get_daily(symbol = 'GOOGL', outputsize='full')
 
-print("data: ", data)
+#df = df[(datetime.datetime(df.dateTimeIndex)<datetime.datetime(2005,1,1))]
+
+columns = ["open", "high", "low", "close", "volume"]
+data.columns = columns
+
+data["TradeDate"] = data.index.date
+
+df = data[data.TradeDate<datetime.date(2005,1,1)]
+
+print(df)
