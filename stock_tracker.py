39233bc9c2eadcd01ec92b9d30172dbef0d47525
michael-rubik <michael.rubik@outlook.at>
2022-02-21 15:34:16 +0100

    save already fetched dataframes as csv

--- stock_tracker.py
+++ stock_tracker.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.pgen import DFAState
 import Constants
 import requests
 import pandas as pd
@@ -5,21 +6,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 from helper_functions import *
-
 from alpha_vantage.timeseries import TimeSeries
+from pathlib import Path
+
+def getFromAPI(symbol):
+    ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+    # Get json object with the intraday data and another with  the call's metadata
+    data, meta_data = ts.get_daily(symbol = symbol, outputsize='full')
 
 
-ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
-# Get json object with the intraday data and another with  the call's metadata
-data, meta_data = ts.get_daily(symbol = 'GOOGL', outputsize='full')
+    columns = ["open", "high", "low", "close", "volume"]
+    data.columns = columns
 
-#df = df[(datetime.datetime(df.dateTimeIndex)<datetime.datetime(2005,1,1))]
+    data["TradeDate"] = data.index.date
 
-columns = ["open", "high", "low", "close", "volume"]
-data.columns = columns
+    data.to_csv(f'{symbol}.csv')
+    return data
 
-data["TradeDate"] = data.index.date
+symbol = 'GOOGL'
+path = Path(f'./{symbol}.csv')
 
-df = data[data.TradeDate<datetime.date(2005,1,1)]
+try:
+    df = pd.read_csv(f'{symbol}.csv')
+except:
+    df = getFromAPI(symbol)
 
-print(df)
+df.plot()
+
+plt.show()
